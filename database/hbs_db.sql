6852282e867b45dbe4a837bf9309e5a320314f53
NithishaKumar27 <nithisa04@gmail.com>
2024-05-21 23:37:29 +0530

    b

--- database/hbs_db.sql
+++ database/hbs_db.sql
@@ -12,20 +12,8 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `whbs_db`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `booking_list`
---
 
 CREATE TABLE `booking_list` (
   `id` int(30) NOT NULL,
@@ -41,18 +29,12 @@ CREATE TABLE `booking_list` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `booking_list`
---
+
 
 INSERT INTO `booking_list` (`id`, `code`, `student_id`, `hall_id`, `services_ids`, `schedule`, `total_students`, `remarks`, `status`, `date_created`, `date_updated`) VALUES
 (1, '202201-00001', 2, 2, '|1|,|2|', '2022-02-23', 300, 'This a sample remarks only.', 1, '2022-01-31 14:24:38', '2022-01-31 15:25:54');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `client_list`
---
 
 CREATE TABLE `student_list` (
   `id` int(30) NOT NULL,
@@ -71,19 +53,12 @@ CREATE TABLE `student_list` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `student_list`
---
+
 
 INSERT INTO `student_list` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `password`, `status`, `avatar`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 'Claire', 'C', 'Blake', 'Female', '09123456789', 'This is my sample Address only.', 'cblake@sample.com', '4744ddea876b11dcb1d169fadf494418', 1, 'uploads/clients/1.png?v=1643609613', 0, '2022-01-31 13:33:27', '2022-01-31 14:13:33'),
 (2, 'John', 'D', 'Smith', 'Male', '09123456987', 'This my address.', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 1, '', 0, '2022-01-31 13:34:44', NULL);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `hall_list`
---
 
 CREATE TABLE `hall_list` (
   `id` int(30) NOT NULL,
@@ -98,9 +73,7 @@ CREATE TABLE `hall_list` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `hall_list`
---
+
 
 INSERT INTO `hall_list` (`id`, `code`, `name`,  `description`, `image_path`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1,'KP101', 'KP HALL - 101','TYPE: NORMAL CLASSROOM; LOCATION: GROUND FLOOR; PURPOSE: For lectures, exams and events.',NULL, 1, 0, '2024-05-20 09:59:18', NULL),
@@ -152,11 +125,7 @@ INSERT INTO `hall_list` (`id`, `code`, `name`,  `description`, `image_path`, `st
 
 
 
--- -(1,'KP101', 'KP HALL - 101','TYPE: DIGITAL CLASSROOM; LOCATION: GROUND FLOOR; PURPOSE: For lectures exams and event-------------------------------------------------------
 
---
--- Table structure for table `message_list`
---
 
 CREATE TABLE `message_list` (
   `id` int(30) NOT NULL,
@@ -168,18 +137,10 @@ CREATE TABLE `message_list` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `message_list`
---
 
 INSERT INTO `message_list` (`id`, `fullname`, `contact`, `email`, `message`, `status`, `date_created`) VALUES
 (1, 'John Smith', '09123456789', 'jsmith@sample.com', 'This is a sample inquiry only.', 1, '2022-01-31 15:27:35');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `service_list`
---
 
 CREATE TABLE `service_list` (
   `id` int(30) NOT NULL,
@@ -191,9 +152,7 @@ CREATE TABLE `service_list` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `service_list`
---
+
 
 INSERT INTO `service_list` (`id`, `name`, `description`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 'EXAMINATION', 'First preference is always given for exams,next for lectures and then for events.Incase of any important events,the decision of the admin will be final', 1, 0, '2022-01-31 10:22:31', NULL),
@@ -207,9 +166,7 @@ CREATE TABLE `system_info` (
   `meta_value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `system_info`
---
+
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 (1, 'name', 'AU Hall Booking System'),
@@ -224,11 +181,6 @@ INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 (19, 'to_time', '18:30'),
 (20, 'address', ' Sardar Patel Road, Guindy, Chennai, 600028');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(50) NOT NULL,
@@ -245,116 +197,71 @@ CREATE TABLE `users` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `status`, `date_added`, `date_updated`) VALUES
 (1, 'Adminstrator', NULL, 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1639468007', NULL, 1, 1, '2021-01-20 14:02:37', '2021-12-14 15:47:08'),
 (4, 'Smantha', NULL, 'Lou', 'slou', '1ed1255790523a907da869eab7306f5a', 'uploads/avatar-4.png?v=1641346647', NULL, 2, 1, '2022-01-05 09:36:56', '2022-01-05 09:37:27');
 
---
--- Indexes for dumped tables
---
-  
 
---
--- Indexes for table `booking_list`
---
+
+
 ALTER TABLE `booking_list`
   ADD PRIMARY KEY (`id`),
   ADD KEY `hall_id` (`hall_id`),
   ADD KEY `student_id` (`student_id`);
 
---
--- Indexes for table `student_list`
---
+
 ALTER TABLE `student_list`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `hall_list`
---
+
 ALTER TABLE `hall_list`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `message_list`
---
+
 ALTER TABLE `message_list`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `service_list`
---
+
 ALTER TABLE `service_list`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `system_info`
---
+
 ALTER TABLE `system_info`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `booking_list`
---
 ALTER TABLE `booking_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `client_list`
---
+
 ALTER TABLE `student_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `hall_list`
---
+
 ALTER TABLE `hall_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `message_list`
---
 ALTER TABLE `message_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `service_list`
---
+
 ALTER TABLE `service_list`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `system_info`
---
+
 ALTER TABLE `system_info`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `booking_list`
---
+
 ALTER TABLE `booking_list`
   ADD CONSTRAINT `booking_list_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student_list` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `booking_list_ibfk_2` FOREIGN KEY (`hall_id`) REFERENCES `hall_list` (`id`) ON DELETE CASCADE;
